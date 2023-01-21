662bae3a44f2be83889769fbf33006ea99a0c281
IvanovArtemNA <IvanovArtemNA@gmail.com>
2023-01-21 02:17:06 +0200

    added base files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+import { Filter } from './Filter/Filter';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { selectIsLoading, selectError } from 'redux/selectors';
+import { fetchContacts } from 'redux/operations';
+
 export const App = () => {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div style={{ margin: '0 auto', width: '333px' }}>
+      <h2>Phonebook</h2>
+      <ContactForm />
+      <h2>Contacts</h2>
+      <Filter />
+      {!isLoading && !error && <ContactList />}
     </div>
   );
 };
---
+++ src/components/ContactList/ContactList.js
@@ -0,0 +1,30 @@
+import css from './ContactList.module.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectContacts, selectFilter } from 'redux/selectors';
+import { deleteContact } from 'redux/operations';
+
+export const ContactList = () => {
+  const dispatch = useDispatch();
+  const contacts = useSelector(selectContacts);
+  const filter = useSelector(selectFilter);
+  const getContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
+  return (
+    <ul className={css.list}>
+      {getContacts.map(({ id, name, number }) => (
+        <li key={id} className={css.item}>
+          {name + ': ' + number}
+          <button
+            type="button"
+            className={css.remove}
+            onClick={() => dispatch(deleteContact(id))}
+          >
+            Delete
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+};
